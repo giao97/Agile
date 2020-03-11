f643f92c3208b781a9fb57be5427fa0ba8d787b6
marcus-xjtu <marcus704@stu.xjtu.edu.cn>
2020-03-11 17:24:26 +0800

    initial status

---
+++ poker/src/TexasPoker.java
@@ -0,0 +1,42 @@
+public class TexasPoker {
+    public static String CheckWinner(String Black, String White){
+        String result = "";
+
+        return result;
+    }
+
+    public static int CheckPokerType(String Pks){
+        //判断牌的类型
+        int type = 0;
+
+        return type;
+    }
+
+    public static boolean CheckFlush(String Pks){
+        //判断花色
+        boolean Flush = false;
+
+        return Flush;
+    }
+
+    public static boolean CheckStraight(String Pks){
+        //判断顺子
+        boolean Straight = false;
+
+        return Straight;
+    }
+
+    public static int CheckKinds(String Pks){
+        //判断相同点数个数
+        int kind = 0;
+
+        return kind;
+    }
+
+    public static int CheckPairs(String Pks){
+        //判断对子个数
+        int pairs = 0;
+
+        return pairs;
+    }
+}
---
+++ poker/src/TexasPokerTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+class TexasPokerTest {
+
+    @Test
+    public void checkWinner() {
+    }
+
+    @Test
+    public void checkPokerType() {
+    }
+
+    @Test
+    public void checkFlush() {
+    }
+
+    @Test
+    public void checkStraight() {
+    }
+
+    @Test
+    public void checkKinds() {
+    }
+
+    @Test
+    public void checkPairs() {
+    }
+}
