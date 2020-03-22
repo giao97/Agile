2b24934f737418603fd12bf97febc185e51fb230
marcus-xjtu <marcus704@stu.xjtu.edu.cn>
2020-03-22 13:13:09 +0800

    create newtask

--- crud/src/controller.js
+++ crud/src/controller.js
@@ -19,4 +19,18 @@ exports.getAllTasks = (req, res) => fs.readFile(req.app.locals.dataFilePath, "ut
   res.send(JSON.parse(data))
 })
 
+exports.createTask = async (req, res) => {
+  const newTask = req.body
+  const file = await asyncReadFile(req.app.locals.dataFilePath)
+  const tasks = JSON.parse(file)
+  if (tasks.filter(function (e) { return e.id == newTask.id; }).length != 0) {
+    res.status(400).send()
+  } else {
+    newTask.createdTime = new Date()
+    tasks.push(newTask)
+    await asyncWriteFile(JSON.stringify(tasks), req.app.locals.dataFilePath)
+    res.status(201).send(tasks)
+  }
+}
+
 
--- crud/test/app-spec.js
+++ crud/test/app-spec.js
@@ -39,6 +39,56 @@ const {
       })
     })
   
-    
+    describe("post request", () => {
+      afterEach(async function () {
+        await asyncWriteFile(JSON.stringify([{
+            "id": 1,
+            "content": "Restful API homework",
+            "createdTime": "2019-05-15T00:00:00Z"
+          },
+          {
+            "id": 2,
+            "content": "Restful API homework",
+            "createdTime": "2019-05-15T00:00:00Z"
+          }
+        ]), "./test/fixture.json")
+      })
+      it("should create a task when the corresponding id does not exist in the datasource", (done) => {
+        request(app).post('/tasks').send({
+            "id": 3,
+            "content": "Restful API homework",
+            "createdTime": "2019-05-15T00:00:00Z"
+        }).expect(201).expect([{
+            "id": 1,
+            "content": "Restful API homework",
+            "createdTime": "2019-05-15T00:00:00Z"
+          },
+          {
+            "id": 2,
+            "content": "Restful API homework",
+            "createdTime": "2019-05-15T00:00:00Z"
+          },
+          {
+            "id": 3,
+            "content": "Restful API homework",
+            "createdTime": "2019-05-15T00:00:00Z"
+          }
+        ]).end((err, res) => {
+          if (err) throw err;
+          done()
+        })
+      })
+  
+      it("should not create the task when its id has already existed in the datasource", (done) => {
+        request(app).post('/tasks').send({
+            "id": 1,
+            "content": "Restful API homework",
+            "createdTime": "2019-05-15T00:00:00Z"
+        }).expect(400).end((err, res) => {
+          if (err) throw err;
+          done()
+        })
+      })
+    })
   })
   
